4a8921b2576d2f5bd5e406a24986648819c8ae58
nDija <hullaert.vincent@gmail.com>
2014-03-10 10:26:49 +0100

    test(result): show result
    Add show result in console

--- src/test/java/com/github/ndija/acrho_client/service/PDFServiceTest.java
+++ src/test/java/com/github/ndija/acrho_client/service/PDFServiceTest.java
@@ -2,6 +2,7 @@ package com.github.ndija.acrho_client.service;
 
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -12,17 +13,24 @@ import com.github.ndija.acrho_client.exception.AcrhoPDFException;
 
 public class PDFServiceTest {
 
+    private final static Logger log = Logger.getLogger(PDFServiceTest.class);
     @Test
     public void extractTest() throws AcrhoConnectionException, AcrhoPDFException {
         // Given
         String filename = "/Resultats_Bury_220214.pdf";
+        String filename2 ="/Resultats_Dottignies_16022014.pdf";
 
         // When
-        RunResult result = PdfService.extract(filename);
+        RunResult result = PdfService.extract(filename2);
         List<Result> results = result.getResults();
 
         // Then
         Assert.assertNotNull(results);
         Assert.assertNotEquals(results.size(), 0);
+
+        for(Result r : results) {
+            log.info(r.toString());
+        }
+
     }
 }
