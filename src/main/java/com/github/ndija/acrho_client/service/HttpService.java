fb12fd849ab6317537c239f06d679e7fdbb0cff0
Vincent Hullaert <hullaert.vincent@gmail.com>
2016-03-30 15:10:15 +0200

    feat(http): add post method in service

--- src/main/java/com/github/ndija/acrho_client/service/HttpService.java
+++ src/main/java/com/github/ndija/acrho_client/service/HttpService.java
@@ -1,14 +1,19 @@
 package com.github.ndija.acrho_client.service;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.InetSocketAddress;
 import java.net.MalformedURLException;
 import java.net.Proxy;
 import java.net.URL;
 
-import org.apache.commons.lang.StringUtils;
+import javax.net.ssl.HttpsURLConnection;
+
+import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 
 import com.github.ndija.acrho_client.exception.AcrhoConnectionException;
@@ -21,6 +26,8 @@ public class HttpService {
 
     private static Integer proxy_port = 80;
 
+    private final String USER_AGENT = "Mozilla/5.0";
+    
     static {
         proxy_adress = AcrhoProperties.get(AcrhoProperties.PROXY_ADRESS);
         String port = AcrhoProperties.get(AcrhoProperties.PROXY_PORT);
@@ -59,4 +66,44 @@ public class HttpService {
             throw new AcrhoConnectionException("Can't read url: " + url, e);
         }
     }
+    
+    public String post(String url, String data) throws Exception {
+
+		//String url = "https://selfsolve.apple.com/wcResults.do";
+		URL obj = new URL(url);
+		HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+
+		//add reuqest header
+		con.setRequestMethod("POST");
+		con.setRequestProperty("User-Agent", USER_AGENT);
+		con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+
+		//String urlParameters = "sn=C02G8416DRJM&cn=&locale=&caller=&num=12345";
+		
+		// Send post request
+		con.setDoOutput(true);
+		DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+		wr.writeBytes(data);
+		wr.flush();
+		wr.close();
+
+		int responseCode = con.getResponseCode();
+		log.debug("\nSending 'POST' request to URL : " + url);
+		log.debug("Post parameters : " + data);
+		log.debug("Response Code : " + responseCode);
+
+		BufferedReader in = new BufferedReader(
+		        new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+		
+		//print result
+		return response.toString();
+	}
+
 }
