9a0e0d1ec05ed37bcba082eaf99d3ac4cd850309
Vincent Hullaert <hullaert.vincent@gmail.com>
2017-03-17 14:52:15 +0100

    chore(httpPost): add comment and assertion

--- src/test/java/org/acrho/test/acrho_client/HttpPostTest.java
+++ src/test/java/org/acrho/test/acrho_client/HttpPostTest.java
@@ -5,23 +5,25 @@ import java.nio.charset.StandardCharsets;
 
 import org.acrho.acrho_client.service.AcrhoProperties;
 import org.acrho.acrho_client.service.HttpService;
-import org.acrho.acrho_client.service.IAcrhoProperties;
 import org.apache.commons.io.IOUtils;
-import org.apache.log4j.Logger;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Assert;
 import org.junit.Test;
 
 
+/**
+ * Test if post service respond fine
+ * @author Vincent Hullaert
+ *
+ */
 public class HttpPostTest {
-
-	private static final Logger LOG = Logger.getLogger(HttpPostTest.class);
-	
-	public String data = "cle_langue=1011306039&langue=french&cle_menus=1187970092&cle_data=0&adminop=none&ant_order=a.place asc&ant_filter=ant_search_courses&ant_filter_value=441&selected_anchor=0";
 	
 	@Test
 	public void postTest() throws Exception {
-		String url = AcrhoProperties.get(IAcrhoProperties.URL_RESULT_RUN);
-		String parameters = AcrhoProperties.get(IAcrhoProperties.URL_RESULT_RUN_PARAMETERS);
+		String url = AcrhoProperties.get(AcrhoProperties.URL_RESULT_RUN);
+		String parameters = AcrhoProperties.get(AcrhoProperties.URL_RESULT_RUN_PARAMETERS);
 		InputStream result = HttpService.post(url, parameters);
-		LOG.debug(IOUtils.toString(result, StandardCharsets.UTF_8.name()));
+		String sResult = IOUtils.toString(result, StandardCharsets.UTF_8.name());
+		Assert.assertTrue("Result is empty", StringUtils.isNotEmpty(sResult));
 	}
 }
